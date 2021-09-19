f0a523e22f0bb5ad0e0b2fb3a580c0799cfea4ab
Gregory-hub <novikov.gri@gmail.com>
2021-09-19 13:32:28 +0300

    init

---
+++ cpp/09_sept/lections/17-09.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	// float x = 12.1;
+	// printf("%f, %c, %s\n", x, 'a', "asdasd1");
+	// printf("Biba %.2f\n", x);
+
+	float y;
+	char q;
+	scanf("%f %c", &y, &q);
+	printf("%f, %c", y, q);
+
+	return 0;
+}
