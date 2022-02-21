ca1eac0b9310dfc729c09f88b57a8b6932cfb0ee
Gregory-hub <novikov.gri@gmail.com>
2022-02-22 00:50:50 +0300

    Improved lab1

--- cpp/2 sem/1/1.cpp
+++ cpp/2 sem/1/1.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
+
 
 using namespace std;
 
 
+const int SIZE = 80;
+
 struct Word {
-    static const int sz = 80;
+    static const int sz = SIZE;
     char letters[sz + 1];
-    char marker;
+    char marker = 0;
 
     Word() {
         for (int i = 0; i <= sz; i++) {
-            letters[i] = '0';
+            letters[i] = '.';
         }
+        letters[sz] = marker;
     }
 };
 
 
-int get_number_of_lines(string filename) {
+int count_lines(string filename) {
     fstream file;
     file.open(filename, ios::in);
     if (!file.is_open()) {
@@ -62,7 +67,7 @@ int get_number_of_els_in_line(string filename, int line_index = 0) {
 
 
 int read_word_with_len(string filename, Word& word, int line_index = 0) {
-    int number_of_lines = get_number_of_lines(filename);
+    int number_of_lines = count_lines(filename);
     if (line_index >= number_of_lines) {
         cerr << "Error: trying to read non-existing or empty line" << endl;
     }
@@ -116,7 +121,7 @@ int read_word_with_len(string filename, Word& word, int line_index = 0) {
     word.marker = x.c_str()[0];
 
     // read word
-    getline(ss, x, ' ');
+    getline(ss, x);
     if (x.length() < len) {
         for (int i = 0; i < x.length(); i++) {
             word.letters[i] = x[i];
@@ -135,7 +140,7 @@ int read_word_with_len(string filename, Word& word, int line_index = 0) {
 
 
 char read_word_with_sep(string filename, Word& word, int line_index = 0) {
-    int number_of_lines = get_number_of_lines(filename);
+    int number_of_lines = count_lines(filename);
     if (line_index >= number_of_lines) {
         cerr << "Error: trying to read non-existing or empty line" << endl;
     }
@@ -189,33 +194,108 @@ char read_word_with_sep(string filename, Word& word, int line_index = 0) {
 }
 
 
-int exchange(Word &word, char* removed, char* inserted, int len) {
-    char seq[len];
-    int exc_count = 0;
+void read_substrs(string filename, Word subs[][2], int len, char marker) {
+    fstream file;
+    file.open(filename, ios::in);
+    if (!file.is_open()) {
+        perror("Error opening file");
+        exit(1);
+    }
 
-    int i = 0;
+    string line;
     for (int i = 0; i < len; i++) {
-        if (word.letters[i] == word.marker) return 0;
+        int els_count = get_number_of_els_in_line(filename, i);
+        if(els_count < 2) {
+            cerr << "Too little substrings in line " << i + 1 << endl;
+            exit(1);
+        }
+
+        Word sub1;
+        Word sub2;
+        sub1.marker = marker;
+        sub2.marker = marker;
+        sub1.letters[sub1.sz] = marker;
+        sub2.letters[sub2.sz] = marker;
+
+        getline(file, line);
+        int k = 0;
+        while (line[k] != ' ') {
+            sub1.letters[k] = line[k];
+            k++;
+        }
+        sub1.letters[k] = marker;
+        int l = k + 1;
+
+        while (line[l] && line[l] != ' ') {
+            sub2.letters[l - k - 1] = line[l];
+            l++;
+        }
+        sub2.letters[l - k - 1] = marker;
+
+        subs[i][0] = sub1;
+        subs[i][1] = sub2;
     }
 
-    while (word.letters[i + len - 1] != word.marker and i < word.sz - len) {
-        for (int j = i; j < i + len; j++) {
-            seq[j - i] = word.letters[j];
-        }
-        bool equal = true;
-        for (int i = 0; i < len; i++) {
-            if (seq[i] != removed[i]) equal = false;
-        }
-        if (equal) {
-            for (int j = i; j < i + len; j++) {
-                word.letters[j] = inserted[j - i];
+    file.close();
+}
+
+
+int find_substr(const Word &word, const Word &sub, int word_len, int sub_len, int search_start = 0) {
+    if(sub_len == 0) return -1;
+    bool is_equal = true;
+    for(int i = search_start; i <= word_len - sub_len; i++) {
+        is_equal = true;
+        for(int j = 0; j < sub_len; j++) {
+            if(sub.letters[j] != word.letters[i + j]) {
+                is_equal = false;
+                break;
             }
-            i += len - 1;
-            exc_count++;
         }
-        i++;
+        if(is_equal) {
+            return i;
+        }
     }
-    return exc_count;
+    return -1;
+}
+
+
+void replace(Word &word, const Word &removed, const Word &inserted) {
+    int word_len = 0;
+    int removed_len = 0;
+    int inserted_len = 0;
+    while (word.letters[word_len] != word.marker) {
+        word_len++;
+    }
+    while (removed.letters[removed_len] != removed.marker) {
+        removed_len++;
+    }
+    while (inserted.letters[inserted_len] != inserted.marker) {
+        inserted_len++;
+    }
+
+    int i = find_substr(word, removed, word_len, removed_len);
+    while(i != -1) {
+        int shift = inserted_len - removed_len;
+        if(word_len + shift > word.sz) {
+            cerr << "Error: word length after replacement is bigger than max size" << endl;
+            exit(1);
+        }
+        if(shift > 0) {
+            for(int k = word_len - 1; k >= i; k--) {
+                word.letters[k + shift] = word.letters[k];
+            }
+        } else if(shift < 0) {
+            for(int k = i; k < word_len + shift; k++) {
+                word.letters[k] = word.letters[k - shift];
+            }
+        }
+        for (int k = i; k < i + inserted_len; k++) {
+            word.letters[k] = inserted.letters[k - i];
+        }
+        word.letters[word_len + shift] = word.marker;
+        word_len += shift;
+        i = find_substr(word, removed, word_len, removed_len, i + inserted_len);
+    }
 }
 
 
@@ -328,31 +408,30 @@ void out_res(string filename, Word word, bool app_res = false) {
 }
 
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     cout << "Author: Novikov G. \n"
             "Group: 1302 \n"
             "Start date: 14.02.2022 \n"
             "End date: 15.02.2022 \n"
-            "Version 1.1.1 \n" << endl;
+            "Version 1.1.1\n" << endl;
 
-    Word word1, word2;
-    int len = read_word_with_len("in.txt", word2, 0);
-    char sep = read_word_with_sep("in.txt", word1, 1);
-    out_init_sep("out.txt", word1, sep);
-    out_init_len("out.txt", word2, len, true);
+    Word word;
 
-    char seq1[4] = {'m', 'i', 'n', 'e'};
-    char seq2[4] = {'o', 'u', 'r', 's'};
-    char seq3[2] = {'r', 'd'};
-    char seq4[2] = {'r', 'm'};
-    char seq5[1] = {'o'};
-    char seq6[1] = {'a'};
-    exchange(word1, seq1, seq2, 4);
-    exchange(word2, seq3, seq4, 2);
-    exchange(word2, seq5, seq6, 1);
-    out_res("out.txt", word1);
-    out_res("out.txt", word2, true);
+    char sep = read_word_with_sep("in.txt", word);
+    out_init_sep("out.txt", word, sep);
+
+    // int len = read_word_with_len("in.txt", word);
+    // out_init_len("out.txt", word, len);
+
+    int subs_count = count_lines("substrings.txt");
+    Word subs[subs_count][2];
+
+    read_substrs("substrings.txt", subs, subs_count, word.marker);
+    for (int i = 0; i < subs_count; i++) {
+        replace(word, subs[i][0], subs[i][1]);
+    }
+
+    out_res("out.txt", word);
 
     return 0;
 }
