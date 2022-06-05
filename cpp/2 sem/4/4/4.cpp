f1e258fe584a61011eb92ba840548431a86561a5
Gregory-hub <novikov.gri@gmail.com>
2022-06-05 19:06:11 +0300

    Add my name in the beginnig

--- cpp/2 sem/4/4/4.cpp
+++ cpp/2 sem/4/4/4.cpp
@@ -3,6 +3,11 @@
 
 int main()
 {
+	cout << "Author: Novikov G.V.\n"
+		"Start date: 01.06.2022\n"
+		"End date: 05.06.2022\n"
+		"Version: 4.01\n" << endl;
+
 	All* all = new All;
 	all->read("in1.txt", "in2.txt", "in3.txt");
 	FormV* diff = subtract(all->list1, all->list2);
