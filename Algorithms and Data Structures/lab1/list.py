2ac7e752f3ab26d45d0b51b1f5b06211402bbbce
Gregory-hub <novikov.gri@gmail.com>
2022-09-26 19:55:05 +0300

    Tabs to spaces so linter dont get angry

--- Algorithms and Data Structures/lab1/list.py
+++ Algorithms and Data Structures/lab1/list.py
@@ -37,15 +37,15 @@ class LinkedList:
         return f"<LinkedList(head={self.head}, size={self.size})>"
 
     def is_empty(self):
-		# O(1)
+        # O(1)
         return self.size == 0
 
     def get_size(self):
-		# O(1)
+        # O(1)
         return self.size
 
     def get(self, index: int):
-		# O(n)
+        # O(n)
         if index >= self.size or index < 0:
             return None
 
@@ -56,7 +56,7 @@ class LinkedList:
         return node
 
     def print(self):
-		# O(n)
+        # O(n)
         node = self.head
         for i in range(self.size):
             if (node is None):
@@ -66,9 +66,9 @@ class LinkedList:
             node = node.next
 
     def insert(self, index: int, value: int):
-		# O(n)
-		# index == 0 => O(1)
-		# index =! 0 => O(n)
+        # O(n)
+        # index == 0 => O(1)
+        # index =! 0 => O(n)
         if not 0 <= index <= self.size:
             print("Error: invalid index")
             return
@@ -78,17 +78,17 @@ class LinkedList:
 
         node = Node(value)
         if index == 0:
-			# O(1)
+            # O(1)
             node.next = self.head
             self.head = node
         elif index == self.size:
-			# O(n)
+            # O(n)
             last_el = self.get(self.size - 1)
             if last_el is None:
                 return
             last_el.next = node
         else:
-			# O(n)
+            # O(n)
             prev_el = self.get(index - 1)
             next_el = self.get(index)
             prev_el.next = node
@@ -97,11 +97,11 @@ class LinkedList:
         self.size += 1
 
     def add_left(self, value: int):
-		# O(1)
+        # O(1)
         self.insert(0, value)
 
     def add_right(self, value: int):
-		# O(n)
+        # O(n)
         if not isinstance(value, int):
             print("Error: value must be int")
             return
@@ -109,7 +109,7 @@ class LinkedList:
         self.insert(self.size, value)
 
     def remove(self, index: int):
-		# O(n)
+        # O(n)
         if not 0 <= index < self.size:
             print("Error: invalid index")
             return
@@ -117,30 +117,30 @@ class LinkedList:
         prev_el = self.get(index - 1)
         next_el = self.get(index + 1)
         if prev_el is None and next_el is None:
-			# O(1)
+            # O(1)
             self.head = None
         elif prev_el is None:
-			# O(1)
+            # O(1)
             self.head = self.get(1)
         elif next_el is None:
-			# O(1)
+            # O(1)
             prev_el.next = None
         else:
-			# O(1)
+            # O(1)
             prev_el.next = next_el
 
         self.size -= 1
 
     def remove_last(self):
-		# O(n)
+        # O(n)
         self.remove(self.size - 1)
 
     def remove_first(self):
-		# O(1)
+        # O(1)
         self.remove(0)
 
     def replace(self, index: int, value: int):
-		# O(n)
+        # O(n)
         if not 0 <= index < self.size:
             print("Error: invalid index")
             return
@@ -149,12 +149,12 @@ class LinkedList:
         self.insert(index, value)
 
     def delete_all(self):
-		# O(n)
+        # O(n)
         while (self.head is not None):
             self.remove_first()
 
     def find_last_list_entrance(self, li: LinkedList):
-		# O(n)
+        # O(n)
         last_entrance = -1
 
         curr_el = self.head
@@ -180,19 +180,19 @@ class LinkedList:
 
 
 def main():
-	li = LinkedList([2, 3, 4, 0, -1, 0, 1, 3, 4, 5, 6, -1, 0, 1, 2, 3, 4, 5, 6, 7])
-	li.remove_first()
-	li.remove_last()
-	li.add_left(0)
-	li.add_right(10)
-	li.insert(3, 13)
-	li.print()
-	print()
-	print("Index of last entrance:", li.find_last_list_entrance(LinkedList([3, 4])))
-	li.delete_all()
-	li.print()
-	print(li.is_empty())
+    li = LinkedList([2, 3, 4, 0, -1, 0, 1, 3, 4, 5, 6, -1, 0, 1, 2, 3, 4, 5, 6, 7])
+    li.remove_first()
+    li.remove_last()
+    li.add_left(0)
+    li.add_right(10)
+    li.insert(3, 13)
+    li.print()
+    print()
+    print("Index of last entrance:", li.find_last_list_entrance(LinkedList([3, 4])))
+    li.delete_all()
+    li.print()
+    print(li.is_empty())
 
 
 if __name__ == "__main__":
-	main()
+    main()
