74173f8d47f228a73070cc29f02627636a39b83c
Gregory-hub <novikov.gri@gmail.com>
2022-11-11 22:40:15 +0300

    Some algirithms

--- Algorithms and Data Structures/lab4/bin_tree.py
+++ Algorithms and Data Structures/lab4/bin_tree.py
@@ -14,30 +14,60 @@ class BinSearchTree:
 		self.root = self.__tree_from_list(elements, len(elements))
 
 	# public:
-	def height(self):
+	def height(self) -> int:
 		# O(n)
 		return self.__get_height(self.root)
 
-	def print_tree(self):
+	def print_tree(self) -> None:
 		# O(n)
 		print(f"Tree of height {self.height()}:")
 		self.__print_tree(self.root)
 		print()
 
-	def min(self):
+	def min(self) -> Node:
 		# O(h)
-		return self.__find_min(self.root)
+		return self.__min(self.root)
 
-	def max(self):
+	def max(self) -> Node:
 		# O(h)
-		return self.__find_max(self.root)
+		return self.__max(self.root)
 
-	def get_nodes_inorder(self):
+	def get_nodes_inorder(self) -> list:
 		# O(n)
-		return self.__traversal_inorder(self.root)
+		return self.__get_nodes_inorder(self.root)
+
+	def get_nodes_preorder(self) -> list:
+		# O(n)
+		return self.__get_nodes_preorder(self.root)
+
+	def get_nodes_postorder(self) -> list:
+		# O(n)
+		return self.__get_nodes_postorder(self.root)
+	
+	def find(self, value: int) -> Node:
+		# O(h)
+		return self.__find(self.root, value)
+	
+	def next_el(self, node: Node):
+		# O(h)
+		if node is None:
+			return None
+		if node.right is not None:
+			return self.__min(node.right)
+
+		return self.__get_next_in_ancestors(self.root, node)
+
+	def prev_el(self, node: Node):
+		# O(h)
+		if node is None:
+			return None
+		if node.left is not None:
+			return self.__max(node.left)
+
+		return self.__get_prev_in_ancestors(self.root, node)
 
 	# private:
-	def __get_height(self, root: Node):
+	def __get_height(self, root: Node) -> int:
 		# O(n)
 		if root is None:
 			return 0
@@ -48,7 +78,7 @@ class BinSearchTree:
 		height_r = self.__get_height(root.right)
 		return max(height_l, height_r) + 1
 
-	def __tree_from_list(self, elements: list, n: int):
+	def __tree_from_list(self, elements: list, n: int) -> Node:
 		# elements must be sorted
 		# O(n)
 		if n == 1:
@@ -63,7 +93,7 @@ class BinSearchTree:
 		root.right = right
 		return root
 
-	def __print_tree(self, root: Node, level: int = 0, child_letter = ''):
+	def __print_tree(self, root: Node, level: int = 0, child_letter = '') -> None:
 		# O(n)
 		if root is None:
 			return
@@ -73,32 +103,103 @@ class BinSearchTree:
 		self.__print_tree(root.left, level + 1, 'l')
 		self.__print_tree(root.right, level + 1, 'r')
 
-	def __find_min(self, root: Node):
+	def __min(self, root: Node) -> Node:
 		# O(h)
 		if root.left is None:
 			return root
-		return self.__find_min(root.left)
+		return self.__min(root.left)
 
-	def __find_max(self, root: Node):
+	def __max(self, root: Node) -> Node:
 		# O(h)
 		if root.right is None:
 			return root
-		return self.__find_max(root.right)
+		return self.__max(root.right)
 
-	def __traversal_inorder(self, root: Node):
+	def __get_nodes_inorder(self, root: Node) -> list:
+		# O(n)
+		if root is None:
+			return []
+
+		left = self.__get_nodes_inorder(root.left)
+		right = self.__get_nodes_inorder(root.right)
+
+		return left + [root] + right 
+
+	def __get_nodes_preorder(self, root: Node) -> list:
+		# O(n)
+		if root is None:
+			return []
+
+		left = self.__get_nodes_preorder(root.left)
+		right = self.__get_nodes_preorder(root.right)
+
+		return [root] + left + right 
+
+	def __get_nodes_postorder(self, root: Node) -> list:
 		# O(n)
 		if root is None:
 			return []
 		
-		left = self.__traversal_inorder(root.left)
-		right = self.__traversal_inorder(root.right)
+		left = self.__get_nodes_postorder(root.left)
+		right = self.__get_nodes_postorder(root.right)
 
 		return left + right + [root]
 
+	def __find(self, root: Node, value: int) -> Node:
+		# O(h)
+		if root is None:
+			return None
 
-li = [0, 1, -39, -212, 57, 22, -190, 30, -41]
-tree = BinSearchTree(li)
-tree.print_tree()
-print(tree.min().value)
-print(tree.max().value)
-print(tree.get_nodes_inorder())
+		if value == root.value:
+			return root
+		if value < root.value:
+			return self.__find(root.left, value)
+		if value > root.value:
+			return self.__find(root.right, value)
+	
+	def __get_next_in_ancestors(self, root: Node, node: Node):
+		# O(h)
+		next_el = None
+		while root and root.value != node.value:
+			if root.value > node.value:
+				next_el = root
+				root = root.left
+			elif root.value < node.value:
+				root = root.right
+		return next_el
+	
+	def __get_prev_in_ancestors(self, root: Node, node: Node):
+		# O(h)
+		next_el = None
+		while root and root.value != node.value:
+			if root.value < node.value:
+				next_el = root
+				root = root.right
+			elif root.value > node.value:
+				root = root.left
+		return next_el
+
+
+		# 		   1
+		# 	     /   \
+		#  	   -1      3
+		#     / \     / \
+		#   -2   0   2   4
+		#   /
+		# -3   
+
+if __name__ == "__main__":
+	li = [-1, -2, -3, 0, 1, 2, 3, 4]
+	tree = BinSearchTree(li)
+	value_to_find = 1
+	el = tree.root.left.right
+
+	tree.print_tree()
+	print("Min:", tree.min())
+	print("Max:", tree.max())
+	print("Traversal preorder:", [i.value for i in tree.get_nodes_preorder()])
+	print("Traversal inorder:", [i.value for i in tree.get_nodes_inorder()])
+	print("Traversal postorder:", [i.value for i in tree.get_nodes_postorder()])
+	print(f"Find {value_to_find}:", tree.find(value_to_find))
+	print(f"Next to {el.value}:", tree.next_el(el))
+	print(f"Previous to {el.value}:", tree.prev_el(el))
