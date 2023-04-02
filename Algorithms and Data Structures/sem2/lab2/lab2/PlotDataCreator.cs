2ed5640e1941e6bb7669a63ff25eb42633bdfe68
Gregory-hub <novikov.gri@gmail.com>
2023-04-02 21:12:11 +0300

    Finish

--- Algorithms and Data Structures/sem2/lab2/lab2/PlotDataCreator.cs
+++ Algorithms and Data Structures/sem2/lab2/lab2/PlotDataCreator.cs
@@ -25,7 +25,7 @@ class PlotDataCreator
 		for (int i = 0; i < size; i++)
 		{
 			for (int j = 0; j < size; j++) {
-				if (i == j || random.Next(0, 101) > connectivity) matrix[i,j] = Double.PositiveInfinity;
+				if (i == j || random.Next(0, 101) <= connectivity) matrix[i,j] = Double.PositiveInfinity;
 				else matrix[i,j] = random.Next(1, 1001);
 			}
 		}
--- Algorithms and Data Structures/sem2/lab2/lab2/Program.cs
+++ Algorithms and Data Structures/sem2/lab2/lab2/Program.cs
@@ -4,7 +4,6 @@ class Program {
     static void Main(string[] args)
     {
         // adjacency matrix
-
         double inf = Double.PositiveInfinity;
         double[,] graph = {
             {0, 2, inf, 1, 4},
@@ -13,26 +12,28 @@ class Program {
             {inf, 1, inf, 0, 3},
             {2, inf, inf, inf, 0}
         };
-        // int source = 0;
+        int source = 3;
 
-        // Dijkstra algorithm = new Dijkstra();
-        // algorithm.SetMatrix(graph);
-        // double[] result = algorithm.Run(source, HeapType.Binary);
-        // for (int i = 0; i < result.Length; i++) {
-        //     Console.Write(Convert.ToString(result[i]) + ' ');
-        // }
-        // Console.WriteLine();
+        Dijkstra algorithm = new Dijkstra();
+        algorithm.SetMatrix(graph);
+        double[] result = algorithm.Run(source, HeapType.Binary);
+        Console.Write("Binary heap: ");
+        for (int i = 0; i < result.Length; i++) {
+            Console.Write(Convert.ToString(result[i]) + ' ');
+        }
+        Console.WriteLine();
 
-        // result = algorithm.Run(source, HeapType.Fibonacci);
-        // for (int i = 0; i < result.Length; i++) {
-        //     Console.Write(Convert.ToString(result[i]) + ' ');
-        // }
-        // Console.WriteLine();
+        result = algorithm.Run(source, HeapType.Fibonacci);
+        Console.Write("Fibonacci heap: ");
+        for (int i = 0; i < result.Length; i++) {
+            Console.Write(Convert.ToString(result[i]) + ' ');
+        }
+        Console.WriteLine();
 
-        PlotDataCreator dataCreator = new PlotDataCreator("data.txt");
-        Console.WriteLine("Running...");
-        dataCreator.CreatePlotData(20, 800);
-        Console.WriteLine("Success!");
+        // PlotDataCreator dataCreator = new PlotDataCreator("data.txt");
+        // Console.WriteLine("Running...");
+        // dataCreator.CreatePlotData(15, 800);
+        // Console.WriteLine("Success!");
 
         Console.ReadLine();
     }
