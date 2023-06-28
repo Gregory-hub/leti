eb1fa99bb69644f3016b301b04e9b47a08b50d50
Gregory-hub <novikov.gri@gmail.com>
2023-06-28 23:31:37 +0300

    OOP cursed

---
+++ OOP/dif_test/example/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace example
+{
+    class Example
+    {
+        private int Value;
+        public Example(int value)
+        {
+            Value = value;
+        }
+
+        public static int operator- (Example a, Example b)
+        {
+            return b.Value - a.Value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example a = new Example(10);
+            Example b = new Example(4);
+            Console.WriteLine(a - b);
+        }
+    }
+}
